2c61f2bb7fcca59438e6ca47a731602126d60932
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2023-07-15 02:00:10 -0300

    altera método 'readAll' e define explicitamente para somente leitura

--- src/main/java/com/gabriel_torelo/game_list/services/GameService.java
+++ src/main/java/com/gabriel_torelo/game_list/services/GameService.java
@@ -3,7 +3,10 @@ package com.gabriel_torelo.game_list.services;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.gabriel_torelo.game_list.dto.GameLongDTO;
 import com.gabriel_torelo.game_list.dto.GameMinDTO;
+import com.gabriel_torelo.game_list.dto.GameShortDTO;
 import com.gabriel_torelo.game_list.entities.Game;
 import com.gabriel_torelo.game_list.repositories.GameRepository;
 
@@ -13,6 +16,7 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> readAll() {
         List<Game> rGames = gameRepository.findAll();
 
