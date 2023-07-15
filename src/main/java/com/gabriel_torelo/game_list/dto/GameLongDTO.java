934777d2e295e29a9cd43003ba3fdc79c62811eb
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2023-07-15 01:30:07 -0300

    cria DTO com quase todas as informações dos jogos (Long - Game Description)

---
+++ src/main/java/com/gabriel_torelo/game_list/dto/GameLongDTO.java
@@ -0,0 +1,86 @@
+package com.gabriel_torelo.game_list.dto;
+
+import org.springframework.beans.BeanUtils;
+import com.gabriel_torelo.game_list.entities.Game;
+
+public class GameLongDTO {
+    private Long id;
+    private String title;
+    private Integer year;
+    private String gender;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String longDescription;
+
+    public GameLongDTO() {
+    }
+
+    public GameLongDTO(Game entity) {
+        BeanUtils.copyProperties(entity, this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+}
