b831c93cbe440e89b198170f458890b0c1e3bede
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2023-07-15 02:18:09 -0300

    cria os métodos 'readID' e 'readMoreID' que retornam os DTOs do 'GameService'

--- src/main/java/com/gabriel_torelo/game_list/controllers/GameController.java
+++ src/main/java/com/gabriel_torelo/game_list/controllers/GameController.java
@@ -3,9 +3,12 @@ package com.gabriel_torelo.game_list.controllers;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.gabriel_torelo.game_list.dto.GameLongDTO;
 import com.gabriel_torelo.game_list.dto.GameMinDTO;
+import com.gabriel_torelo.game_list.dto.GameShortDTO;
 import com.gabriel_torelo.game_list.services.GameService;
 
 @RestController
@@ -18,5 +21,15 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> readAll() {
         return gameService.readAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameShortDTO readID(@PathVariable Long id) {
+        return gameService.readID(id);
+    }
+
+    @GetMapping(value = "/{id}/more_about")
+    public GameLongDTO readMoreID(@PathVariable Long id) {
+        return gameService.readMoreID(id);
     }
 }
