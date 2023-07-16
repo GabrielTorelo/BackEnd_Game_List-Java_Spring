cbf285a35c2c0a4a94d65f1447dbc50cc9a1d706
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2023-07-16 07:50:26 -0300

    cria método 'readListID' que retorna o DTO do 'GameService' com todos os jogos de uma lista específica

--- src/main/java/com/gabriel_torelo/game_list/controllers/GameListController.java
+++ src/main/java/com/gabriel_torelo/game_list/controllers/GameListController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.gabriel_torelo.game_list.dto.GameListDTO;
+import com.gabriel_torelo.game_list.dto.GameMinDTO;
 import com.gabriel_torelo.game_list.services.GameListService;
+import com.gabriel_torelo.game_list.services.GameService;
 
 @RestController
 @RequestMapping(value = "/lists")
@@ -15,6 +17,9 @@ public class GameListController {
     
     @Autowired
     private GameListService gameListService;
+
+    @Autowired
+    private GameService gameService;
 
     @GetMapping
     public List<GameListDTO> readAll() {
@@ -24,5 +29,10 @@ public class GameListController {
     @GetMapping(value = "/{id}")
     public GameListDTO readID(@PathVariable Long id) {
         return gameListService.readID(id);
+    }
+
+    @GetMapping(value = "/{id}/games")
+    public List<GameMinDTO> readListID(@PathVariable Long id) {
+        return gameService.readListID(id);
     }
 }
