4e94f7cd4e0b75dd36c08442474e3d3af0619582
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2023-07-17 20:22:43 -0300

    cria consulta (query) ao BD que atualiza a posição de um jogos dentro de uma lista específica

--- src/main/java/com/gabriel_torelo/game_list/repositories/GameListRepository.java
+++ src/main/java/com/gabriel_torelo/game_list/repositories/GameListRepository.java
@@ -1,8 +1,18 @@
 package com.gabriel_torelo.game_list.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import com.gabriel_torelo.game_list.entities.GameList;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
     
+    @Modifying
+    @Query(nativeQuery = true, value = """
+        UPDATE tb_belonging 
+        SET position = :newIndex 
+        WHERE list_id = :listID 
+        AND game_id = :gameID
+            """)
+    void moveGameInList(Long listID, Long gameID, Integer newIndex);
 }
